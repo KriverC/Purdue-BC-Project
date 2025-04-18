12b2cb93b2883087c30ea5570914e8d4b969a9c6
KeeganRivCox <nageekr@gmail.com>
2025-04-18 16:47:15 -0400

    Finished Market contract

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -1,76 +1,104 @@
-pragma solidity ^0.8.24
+pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-  
-contract Market {
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract Market is ReentrancyGuard, IERC721Receiver {
+    
+  using Address for address payable;
 
   uint256 public nextId;
-  mapping(uint256 => item) items;
+  mapping(uint256 => Listing) public listings;
 
-  struct Item {
+  struct Listing{
     address seller;
-    address nft;
+    address nftAddr;
     uint256 nftId;
     uint256 price;
-    bool forSale;
   }
-  
-  // Event to log items listed for sale on the martket
-  event Sale(address indexed nft, address indexed seller, uint256 nftId, uint256 itemId, uint256 price);
 
-  function listForSale(address nft, uint256 nftId, uint256 price) external nonReentrant {
+  enum Status {Listed, Bought, Cancelled}
+  
+  // Function to confirm safe handling of nft tokens
+  function onERC721Received(
+    address,      // operator
+    address,      // from
+    uint256,      // tokenId
+    bytes calldata  /* data */
+  ) external pure override returns (bytes4) {
+    // Return the selector to confirm the transfer
+    return IERC721Receiver.onERC721Received.selector;
+  } 
+
+  // One event for all occurences for friendly logging 
+  event Update(
+    uint256 indexed listingId,
+    Status indexed status,
+    address indexed nftAddr,
+    uint256 nftId,
+    address seller,
+    address buyer,
+    uint256 price
+  );
+
+  function listForSale(address nftAddr, uint256 nftId, uint256 price) external nonReentrant {
 
     // Check to see if listing price is valid
-    require(price >= 0, "Price can not be less than zero");
+    require(price > 0, "Price can not be zero or less");
 
     // Transfer ownership of item to contract for holding
-    IERC721(nft).safeTransferFrom(msg.sender, address(this), nftId);
+    IERC721(nftAddr).safeTransferFrom(msg.sender, address(this), nftId);
 
-    // Log listing in contract listings and emit Sale event for logging
-    items[nextId] = Item(msg.sender, nft, nftId, price, true);
-    emit Sale(nft, msg.sender, nftId, nextId++, price);
+    // Map listing in to listingId and emit List event for logging
+    listings[nextId] = Listing(msg.sender, nftAddr, nftId, price);
+    emit Update(nextId, Status.Listed, nftAddr, nftId, msg.sender, address(0), price);
+    nextId++;
 
   }
 
-  // Event to log items bought from the market
-  event Bought(address indexed nft, address indexed buyer, uint256 nftId, uint256 price);
+  function buy(uint256 listingId) external payable nonReentrant {
 
-  function buyItem(uint256 itemId) external payable nonReentrant {
-    // Check if itemId is accounted for
-    requre(itemId < nextId && itemId >= 0, "Invalid item ID");
+    Listing memory listing = listings[listingId];
 
-    // Retrieve existing item and check if for sale
-    Item memory item = items[itemId];
-    require(item.forSale == True, "Item is not for sale");
+    // Check if listing exists
+    require(listing.seller != address(0), "This listing does not exist.");
 
     // Ensure purchaser has provided enough funds to purchase
-    require(msg.value == item.price, "Incorrect funds provided. Ensure to provide the exact amount listed.");
-  
+    require(msg.value == listing.price, "Incorrect funds provided. Ensure to provide the exact amount listed.");
+    
+    // Delete listing from mapping
+    delete listings[listingId];
+
     // Attempt to pay seller 
-    (bool success, ) = item.seller.call{value: msg.value}("");
-    require(success, "Payment failed.")
+    payable(listing.seller).sendValue(msg.value);
   
     // Transfer nft to buyer and emit log
-    IERC721(item.nft).safeTransferFrom(address(this), msg.sender, item.nftId);
-    emit Bought(item.nft, msg.sender, item.nftId, item.price);
+    IERC721(listing.nftAddr).safeTransferFrom(address(this), msg.sender, listing.nftId);
+    emit Update(listingId, Status.Bought, listing.nftAddr, listing.nftId, listing.seller, msg.sender, listing.price);
 
   }
   
-  event Cancel(uint256 listingId);
   
-  function cancelSale(uint256 listingId) {
+  function cancelSale(uint256 listingId) external nonReentrant {
      
-    // Check if listing exists in market
-    require(listingId < nextId && listingId >= 0, "This listing does not exist currently");
-
-    // Retrieve listing and check if sender is seller
+    // Retrieve listing to check if exists and if sender is seller
     Listing memory listing = listings[listingId];
-    require(message.sender == listing.seller, "You must own this listing to cancel");
+    require(listing.seller != address(0), "This listing does not exist.");
+    require(msg.sender == listing.seller, "You must own this listing to cancel");
 
+    // Remove listing
+    delete listings[listingId];
 
+    // Safely transfer nft back to owner
+    IERC721(listing.nftAddr).safeTransferFrom(address(this), listing.seller, listing.nftId);
+
+    // Emit cancellation log
+    emit Update(listingId, Status.Cancelled, listing.nftAddr, listing.nftId, listing.seller, address(0), listing.price);
 
   }
+
+
 
 }
